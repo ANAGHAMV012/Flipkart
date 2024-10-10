c5b1c5ea82605464510c8a847d7d422883366bda
Anagha <anaghamv012@gmail.com>
2024-10-10 09:00:27 +0530

    flipkart

--- script.js
+++ script.js
@@ -232,8 +232,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 })
                 top.append(loop)
             })
+            
             const right = document.querySelector('.right')
-            right.innerHTML=`
+            right.innerHTML = `
                 <div class="right-one">
                     <div class="right1"></div>
                     <div class="right2"></div>
@@ -244,84 +245,207 @@ document.addEventListener('DOMContentLoaded', () => {
                     </div>
                 </div>
             `
+            
             const righto = document.querySelector('.right1')
             data.right.righto.forEach(item => {
                 const onedi = document.createElement('div')
-                onedi.className='one-di'
-                onedi.innerHTML=`
+                onedi.className = 'one-di'
+                onedi.innerHTML = `
                     <a>${item.title}</a>
                     <img src="${item.arrow}">
                 `
                 righto.append(onedi)
             })
+            
             const righttw = document.querySelector('.right2')
             data.right.righttw.forEach(item => {
                 const twodi = document.createElement('a')
-                twodi.innerHTML=`${item}`
+                twodi.innerHTML = `${item}`
                 righttw.append(twodi)
             })
+            
+            const br = document.createElement('br')
+            righttw.append(br)
+            
             const rightfi = document.querySelector('.right5')
             data.right.rightfive.forEach(item => {
                 const spane = document.createElement('div')
-                spane.className='spane'
-                spane.innerHTML=`${item}`
+                spane.className = 'spane'
+                spane.innerHTML = `${item}`
                 rightfi.append(spane)
             })
-            const br = document.createElement('br')
-            righttw.append(br)
-            data.right.rightmain.forEach(item => {
-                const rightdwn = document.createElement('div')
-                rightdwn.className='right-dwn'
-                rightdwn.innerHTML=`
-                    <div class="rightdwn-sub">
-                        <div class="right-pad">
-                            <a>
-                                <div class="rpadl">
-                                    <div class="rpdl1">
-                                        <img src="${item.img}">
+            
+            // Pagination logic
+            let currentPage = 1;
+            const itemsPerPage = 24;
+            const totalPages = Math.ceil(data.right.rightmain.length / itemsPerPage);
+            
+            function renderPage(page) {
+                const startIndex = (page - 1) * itemsPerPage;
+                const endIndex = startIndex + itemsPerPage;
+            
+                // Clear the right container of any previously rendered items
+                const rightDwnContainer = document.querySelector('.right-dwn-container');
+                if (rightDwnContainer) {
+                    rightDwnContainer.remove();
+                }
+            
+                const container = document.createElement('div')
+                container.className = 'right-dwn-container';
+            
+                // Generate main content in right-dwn for the current page
+                const itemsToRender = data.right.rightmain.slice(startIndex, endIndex);
+                itemsToRender.forEach(item => {
+                    const rightdwn = document.createElement('div')
+                    rightdwn.className = 'right-dwn'
+                    rightdwn.innerHTML = `
+                        <div class="rightdwn-sub">
+                            <div class="right-pad">
+                                <a>
+                                    <div class="rpadl">
+                                        <div class="rpdl1"><img src="${item.img}"></div>
+                                        <div class="rpdl2">
+                                            <span>
+                                                <label id="label1">
+                                                    <input>
+                                                    <div class="sung"></div>
+                                                </label>
+                                            </span>
+                                            <label id="label2"><span>${item.bot}</span></label>
+                                        </div>
+                                        <div class="rpdl3"><div class="rpd-sub"><img src="${item.heart}"></div></div>
                                     </div>
-                                    <div class="rpdl2">
-                                        <span>
-                                            <label id="label1">
-                                                <input>
-                                                <div class="sung"></div>
-                                            </label>
-                                        </span>
-                                        <label id="label2">
-                                            <span>${item.bot}</span>
-                                        </label>
-                                    </div>
-                                    <div class="rpdl3">
-                                        <div class="rpd-sub">
-                                            <svg></svg>
+                                    <div class="rpadr">
+                                        <div class="rpadr1">
+                                            <div class="sponser">${item.head}</div>
+                                            <div class="name">${item.title}</div>
+                                            <div class="next">
+                                                <span class="rate">
+                                                    <div>${item.rate}<img src="${item.star}"></div>
+                                                </span>
+                                                <span class="rating">${item.rating}</span>
+                                            </div>
+                                            <div class="ulli"></div>
+                                        </div>
+                                        <div class="rpadr2">
+                                            <div class="pad1">
+                                                <div class="pad1up">
+                                                    <div class="apn">${item.price}</div>
+                                                    <div class="bpn">${item.off}</div>
+                                                    <div class="cpn"><span>${item.discount}</span></div>
+                                                </div>
+                                                <div class="pad2dwn"><div class="dwsub">${item.free}</div></div>
+                                            </div>
+                                            <div class="pad2"><img src="${item.flip}"></div>
+                                            <div class="pad3">
+                                                <div class="pad3-sub"><div class="airt">${item.airtel}</div></div>
+                                            </div>
+                                            <div class="pad4">
+                                                <div class="air-cont">
+                                                    <div class="air">Upto</div>
+                                                    <div class="air">${item.exchange}</div>
+                                                    <div class="air">Off on Exchange</div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="rpadr">
-                                    <div class="rpadr1">
-                                        <div></div>
-                                        <div class="name">${item.title}</div>
-                                        <div class="next"></div>
-                                        <div class="ulli"></div>
-                                    </div>
-                                    <div class="rpadr2">
-                                        <div class="pad1"></div>
-                                        <div class="pad2"></div>
-                                        <div class="pad3"></div>
-                                        <div class="pad4"></div>
-                                    </div>
-                                </div>
-                            </a>
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                `
-                const ulli = rightdwn.querySelector('.ulli')
-                item.li.forEach(listItem => {
-                    const uls = document.createElement('ul')
-                    uls.innerHTML=`<li>${listItem}</li>`
-                    ulli.append(uls)
+                    `
+            
+                    const ulli = rightdwn.querySelector('.ulli')
+                    item.li.forEach(listItem => {
+                        const uls = document.createElement('ul')
+                        uls.innerHTML = `<li>${listItem}</li>`
+                        ulli.append(uls)
+                    })
+            
+                    container.append(rightdwn)
+                })
+            
+                right.append(container)
+            }
+            
+            // Initial render of the first page
+            renderPage(currentPage)
+            
+            // Pagination section (outside loop)
+            const page = document.createElement('div')
+            page.className='page-cont'
+            page.innerHTML = `
+                <div class="page-pad">
+                    <span>Page ${currentPage} of ${totalPages}</span>
+                    <nav class="counter"></nav>
+                    <span class="cyon"></span>
+                </div>
+            `
+            right.append(page)
+            
+            const counter = document.querySelector('.counter')
+            for (let i = 1; i <= totalPages; i++) {
+                const appar = document.createElement('a')
+                appar.innerHTML = `${i}`
+                appar.href = "#"
+                appar.onclick = function(event) {
+                    event.preventDefault()
+                    currentPage = i
+                    renderPage(currentPage)
+                    document.querySelector('.page-pad span').innerText = `Page ${currentPage} of ${totalPages}`
+                }
+                counter.append(appar)
+            }
+            const footer = document.querySelector('.footer');
+            footer.innerHTML = `
+                <div class="footer-sub">
+                    <div class="footup"></div>
+                    <div class="footdwn"></div>
+                </div>
+            `;
+            
+            const footup = document.querySelector('.footup');
+            data.footer.forEach(item => {
+                const bowo = document.createElement('div');
+                bowo.className = 'bowo';
+                bowo.innerHTML = `
+                    <div class="title">${item.title}</div>
+                    <div class="sub"></div>
+                `;
+                footup.append(bowo);
+
+                const sub = bowo.querySelector('.sub');
+                item.sub.forEach(subItem => {
+                    const rit = document.createElement('a');
+                    rit.innerHTML = `${subItem}`;
+                    sub.append(rit);
                 });
-                right.append(rightdwn)
-            })
+            });
+            data.foot.forEach(item => {
+                const bowo = document.createElement('div');
+                bowo.className = 'bowo';
+                bowo.innerHTML = `
+                    <div class="bowo-sub">
+                        <div class="title">${item.title}</div>
+                        <div class="subtitle"></div>
+                        <div class="social">${item.social}</div>
+                    </div>
+                `;
+                footup.append(bowo);
+
+                const sub = bowo.querySelector('.subtitle');
+                item.sub.forEach(subItem => {
+                    const rit = document.createElement('div');
+                    rit.innerHTML = `<p>${subItem}</p>`;
+                    sub.append(rit);
+                });
+
+                const soico = bowo.querySelector('.social-icons.');
+                item.sub.forEach(subItem => {
+                    const rit = document.createElement('div');
+                    rit.innerHTML = `<p>${subItem}</p>`;
+                    soico.append(rit);
+                });
+            });
+            const socialIcons = document.querySelector('.social-icons')
         })
 })
